e44e85d4ae6778dbb05cd217e19a2c5da29d33fd
mattipsaarinen <mattisaarinen@Mattis-MacBook-Pro-M3.local>
2025-01-24 23:27:52 +0200

    data folder

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       targets: [
         {
           src: "src/data/snapshots",
-          dest: "data/snapshots",
+          dest: "data",
         },
       ],
     }),
